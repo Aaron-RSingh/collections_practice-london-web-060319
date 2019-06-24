cbd16e541ed141c3a23542ebb15f967b87e36547
github username <github email address>
2019-06-24 17:40:34 +0100

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort_by(&:length)
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    return array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each {|n| n[2] = "$"}
+end
+
+def find_a(array)
+    array.find_all {|n| n[0] == "a"}
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+    array.map {|word| 
+    if array[1] == word
+        word
+    else
+        word + "s"
+    end
+    }
+end
+
+def add_s_to_all(array)
+    array.map {|n| n + "s"}
+end
